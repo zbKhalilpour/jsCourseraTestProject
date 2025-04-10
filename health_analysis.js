3c646961228f4bd70a377e94c4d76b09e7d376a3
zbKhalilpour <zb.khalilpour@gmail.com>
2025-04-10 02:34:17 -0400

    modified js

--- health_analysis.js
+++ health_analysis.js
@@ -61,3 +61,34 @@ function addPatient() {
   }
 
 addPatientButton.addEventListener("click", addPatient);
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('health_analysis.json')
+      .then(response => response.json())
+      .then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const symptoms = condition.symptoms.join(', ');
+          const prevention = condition.prevention.join(', ');
+          const treatment = condition.treatment;
+
+          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        } else {
+          resultDiv.innerHTML = 'Condition not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+  }
+    btnSearch.addEventListener('click', searchCondition);
